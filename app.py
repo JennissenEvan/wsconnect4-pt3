807dc2ee1141ba6e4e5adad3187633073e54d1cb
JennissenEvan <jennissenevan@gmail.com>
2022-05-18 00:33:08 -0500

    add game logic and event handling

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python
 
 import asyncio
-
 import websockets
+import json
+from connect4 import PLAYER1, PLAYER2, Connect4
 
 
-async def handler(websocket):
+async def handler(websocket: websockets.WebSocketServerProtocol):
+    # Initialize a Connect Four game.
+    game = Connect4()
+
     async for message in websocket:
-        print(message)
+        event = json.loads(message)
+
+        if event["type"] == "play":
+            current_player = PLAYER1 if game.last_player is PLAYER2 else PLAYER2
+            column = event["column"]
+
+            try:
+                row = game.play(current_player, column)
+                out_event = {
+                    "type": "play",
+                    "player": current_player,
+                    "column": column,
+                    "row": row
+                }
+                await websocket.send(json.dumps(out_event))
+
+            except RuntimeError as e:
+                out_event = {
+                    "type": "error",
+                    "message": str(e)
+                }
+                await websocket.send(json.dumps(out_event))
+
+            if game.last_player_won:
+                out_event = {
+                    "type": "win",
+                    "player": game.last_player
+                }
+                await websocket.send(json.dumps(out_event))
 
 
 async def main():
